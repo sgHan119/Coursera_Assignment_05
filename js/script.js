8cf3899c690bef27a5f7c2d5c6ebec7ba54d298f
sgHan <wjkrjob@naver.com>
2017-04-16 21:33:59 +0900

    Assignment05-2nd

--- js/script.js
+++ js/script.js
@@ -118,7 +118,7 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       var homeHtmlToInsertIntoMainPage =
-        "'" + insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName) + "'";
+        insertProperty(homeHtml, "randomCategoryShortName", "'" + chosenCategoryShortName + "'");
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
